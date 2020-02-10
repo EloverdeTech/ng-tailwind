0d01615332c80dd399681027fd0e5448edad425f
Filipe <filipedtristao@gmail.com>
2020-02-10 11:01:30 -0300

    Update lib's public-api

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,10 +2,26 @@
  * Public API Surface of ng-tailwind
  */
 
- // NgtSection
+//NgtSidenav
+export * from './components/ngt-sidenav/ngt-sidenav.module';
+export * from './components/ngt-sidenav/ngt-sidenav.component';
+
+//NgtHeader
+export * from './components/ngt-header-nav/ngt-header-nav.module';
+export * from './components/ngt-header-nav/ngt-header-nav.component';
+
+//NgtContent
+export * from './components/ngt-content/ngt-content.module';
+export * from './components/ngt-content/ngt-content.component';
+
+// NgtSection
 export * from './components/ngt-section/ngt-section.module';
 export * from './components/ngt-section/ngt-section.component';
 
 //NgtInput
 export * from './components/ngt-input/ngt-input.module';
 export * from './components/ngt-input/ngt-input.component';
+
+//NgtStylizable
+export * from './directives/ngt-stylizable/ngt-stylizable.module';
+export * from './directives/ngt-stylizable/ngt-stylizable.directive';
