336d59d997e6dbfbcbe4488d6139a2cdc4d25ae2
wilianx7 <wilianx7@hotmail.com>
2020-02-17 16:58:16 -0300

    Add NgtModalHeader

---
+++ projects/ng-tailwind/src/components/ngt-modal/ngt-modal-header/ngt-modal-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Optional, SkipSelf } from '@angular/core';
+
+import { NgtModalComponent } from '../ngt-modal.component';
+
+@Component({
+  selector: 'ngt-modal-header',
+  templateUrl: './ngt-modal-header.component.html',
+  styleUrls: ['./ngt-modal-header.component.css']
+})
+export class NgtModalHeaderComponent {
+  @Input() public ngtModal: NgtModalComponent;
+
+  constructor(
+    @Optional() @SkipSelf()
+    private ngtModalInstance: NgtModalComponent
+  ) {
+    if (ngtModalInstance) {
+      this.ngtModal = ngtModalInstance;
+    }
+  }
+}
