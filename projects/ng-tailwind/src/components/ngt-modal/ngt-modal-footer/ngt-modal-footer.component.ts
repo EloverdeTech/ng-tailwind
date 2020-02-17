1f94544ce363a6b0b1035b60ce2abbfa408850b2
wilianx7 <wilianx7@hotmail.com>
2020-02-17 16:57:29 -0300

    Add NgtModalFooter

---
+++ projects/ng-tailwind/src/components/ngt-modal/ngt-modal-footer/ngt-modal-footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngt-modal-footer',
+  templateUrl: './ngt-modal-footer.component.html',
+  styleUrls: ['./ngt-modal-footer.component.css']
+})
+export class NgtModalFooterComponent {
+}
