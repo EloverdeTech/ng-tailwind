e0298ca11472a29836e690dad1cb082332045441
wilianx7 <wilianx7@hotmail.com>
2020-02-19 15:14:23 -0300

    Add NgtTbody

---
+++ projects/ng-tailwind/src/components/ngt-datatable/ngt-tbody/ngt-tbody.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[ngt-tbody]',
+  templateUrl: './ngt-tbody.component.html',
+  styleUrls: ['./ngt-tbody.component.css'],
+  host: { class: 'bg-white' }
+})
+export class NgtTbodyComponent {
+  constructor() { }
+}
