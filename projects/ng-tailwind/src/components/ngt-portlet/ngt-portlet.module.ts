2e0c0aa4d1621889b6095cea14d030ff8f141a6d
Filipe <filipedtristao@gmail.com>
2020-02-20 13:43:05 -0300

    Adjust to make NgtPortletHeader use svg icons instead font icons

--- projects/ng-tailwind/src/components/ngt-portlet/ngt-portlet.module.ts
+++ projects/ng-tailwind/src/components/ngt-portlet/ngt-portlet.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { NgtPortletBodyComponent } from './ngt-portlet-body/ngt-portlet-body.component';
 import { NgtPortletFooterComponent } from './ngt-portlet-footer/ngt-portlet-footer.component';
 import { NgtPortletHeaderComponent } from './ngt-portlet-header/ngt-portlet-header.component';
 import { NgtPortletComponent } from './ngt-portlet.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgtSvgModule } from '../ngt-svg/ngt-svg.module';
 
 
 
@@ -24,7 +27,8 @@ import { NgtPortletComponent } from './ngt-portlet.component';
   ],
   imports: [
     CommonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgtSvgModule
   ]
 })
 export class NgtPortletModule { }
