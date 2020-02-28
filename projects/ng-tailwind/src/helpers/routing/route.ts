30ef82ca26bd5f7e3bcb00614ba3f65b09b6325d
wilianx7 <wilianx7@hotmail.com>
2020-02-28 15:21:04 -0300

    Add route helpers

---
+++ projects/ng-tailwind/src/helpers/routing/route.ts
@@ -0,0 +1,12 @@
+import { ActivatedRoute } from '@angular/router';
+import { Observable, zip } from 'rxjs';
+
+export function getIdFromUri(route: ActivatedRoute, routeIdentifier: string) {
+    return Observable.create((observer) => {
+        zip(route.parent.params, route.params)
+            .subscribe((results) => {
+                let params = { ...results[0], ...results[1] };
+                observer.next(params[routeIdentifier]);
+            });
+    });
+}
