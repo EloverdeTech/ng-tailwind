b3d958c6b69b36de81ac3b388f29189e107f3ca1
wilianx7 <wilianx7@hotmail.com>
2020-02-28 15:21:45 -0300

    Add NgtHttpFormService

---
+++ projects/ng-tailwind/src/services/http/ngt-http-form.service.ts
@@ -0,0 +1,7 @@
+import { Observable } from 'rxjs';
+
+export abstract class NgtHttpFormService {
+    abstract loadResourceById(resource: any, resourceId: any): Observable<any>
+
+    abstract saveResource(resource: any): Observable<any>
+}
