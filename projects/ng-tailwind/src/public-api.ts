f9844fa75fb0801b0897e2d0c29b35607e32e963
wilianx7 <wilianx7@hotmail.com>
2020-02-28 17:19:47 -0300

    Update public-api

--- projects/ng-tailwind/src/public-api.ts
+++ projects/ng-tailwind/src/public-api.ts
@@ -99,3 +99,12 @@ export * from './components/ngt-svg/ngt-svg.component';
 //NgtShining
 export * from './components/ngt-shining/ngt-shining.module';
 export * from './components/ngt-shining/ngt-shining.component';
+
+//NgtForm
+export * from './components/ngt-form/ngt-form.module';
+export * from './components/ngt-form/ngt-form.component';
+export * from './components/ngt-form/ngt-form-validation-message/ngt-form-validation-message.module';
+export * from './components/ngt-form/ngt-form-validation-message/ngt-form-validation-message.component';
+
+//NgtHttpFormService
+export * from './services/http/ngt-http-form.service';
