f50d6cf570d76a6bc83172af018d0db018946e68
wilianx7 <wilianx7@hotmail.com>
2020-02-28 15:20:48 -0300

    Add form helpers

---
+++ projects/ng-tailwind/src/helpers/form/form.ts
@@ -0,0 +1,17 @@
+import { NgForm } from '@angular/forms';
+
+export function resetNgForm(ngForm: NgForm) {
+    ngForm.resetForm();
+    setTimeout(() => {
+        ngForm.reset();
+    }, 500);
+}
+
+export function triggerNgFormSubmit(ngForm: NgForm) {
+    ngForm.onSubmit(new Event("submit"));
+}
+
+export function isValidNgForm(ngForm: NgForm) {
+    triggerNgFormSubmit(ngForm);
+    return ngForm.valid;
+}
