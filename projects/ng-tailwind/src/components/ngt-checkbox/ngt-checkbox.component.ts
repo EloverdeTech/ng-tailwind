1cf570a58ec7edfa5a3611e65e9f1127763c52ad
wilianx7 <wilianx7@hotmail.com>
2020-03-02 11:07:29 -0300

    Add NgtStyle -- NgtCheckboxComponent

--- projects/ng-tailwind/src/components/ngt-checkbox/ngt-checkbox.component.ts
+++ projects/ng-tailwind/src/components/ngt-checkbox/ngt-checkbox.component.ts
@@ -1,7 +1,20 @@
-import { AfterViewInit, Component, ElementRef, Host, Input, Optional, Renderer2, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Host,
+  Injector,
+  Input,
+  Optional,
+  Renderer2,
+  Self,
+  ViewChild,
+} from '@angular/core';
 import { ControlContainer, NgForm } from '@angular/forms';
 
 import { NgtBaseNgModel, NgtMakeProvider } from '../../base/ngt-base-ng-model';
+import { NgtStylizableDirective } from '../../directives/ngt-stylizable/ngt-stylizable.directive';
+import { NgtStylizableService } from '../../services/ngt-stylizable/ngt-stylizable.service';
 
 @Component({
   selector: 'ngt-checkbox',
@@ -19,12 +32,28 @@ export class NgtCheckboxComponent extends NgtBaseNgModel implements AfterViewIni
   @Input() label: string;
   @Input() name: string;
 
+  public ngtStyle: NgtStylizableService;
+
   constructor(
+    private injector: Injector,
     @Optional() @Host()
     public formContainer: ControlContainer,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    @Self() @Optional() private ngtStylizableDirective: NgtStylizableDirective
   ) {
     super();
+
+    if (this.ngtStylizableDirective) {
+      this.ngtStyle = this.ngtStylizableDirective.getNgtStylizableService();
+    } else {
+      this.ngtStyle = new NgtStylizableService();
+    }
+
+    this.ngtStyle.load(this.injector, 'NgtCheckbox', {
+      color: {
+        bg: 'gray-500'
+      }
+    });
   }
 
   change(value) {
