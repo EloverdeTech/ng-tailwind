28ba25fc6a3f07aa122d6e60024fa4e60bb3564f
wilianx7 <wilianx7@hotmail.com>
2020-03-03 15:42:37 -0300

    NgtSvg refactoring -- NgtFloatingButtonComponent

--- projects/ng-tailwind/src/components/ngt-floating-button/ngt-floating-button.module.ts
+++ projects/ng-tailwind/src/components/ngt-floating-button/ngt-floating-button.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
+import { NgtSvgModule } from '../ngt-svg/ngt-svg.module';
 import { NgtFloatingButtonComponent } from './ngt-floating-button.component';
 
 
@@ -14,6 +15,7 @@ import { NgtFloatingButtonComponent } from './ngt-floating-button.component';
     CommonModule,
     RouterModule,
     BrowserAnimationsModule,
+    NgtSvgModule
   ]
 })
 export class NgtFloatingButtonModule { }
