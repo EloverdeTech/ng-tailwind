36bd18fe74858c7401134b55eebe4f2164c4e2c8
wilianx7 <wilianx7@hotmail.com>
2020-03-06 09:33:44 -0300

    Add NgtAngularAnimations

---
+++ projects/ng-tailwind/src/animations/ngt-angular-animations.ts
@@ -0,0 +1,19 @@
+import { animate, AnimationStyleMetadata, state, style, transition, trigger } from '@angular/animations';
+
+export function slideLeftToRightAnimation(name: string, duration: any, customStyle?: AnimationStyleMetadata) {
+    return trigger(name, [
+        state('void', customStyle ? customStyle : style({ transform: 'translateX(-50px)', opacity: 0 })),
+        transition(':enter, :leave', [
+            animate(duration)
+        ])
+    ]);
+}
+
+export function fadeDownAnimation(name: string, duration: any, customStyle?: AnimationStyleMetadata) {
+    return trigger(name, [
+        state('void', customStyle ? customStyle : style({ transform: 'translateY(-20px)', opacity: 0 })),
+        transition(':enter, :leave', [
+            animate(duration)
+        ])
+    ])
+}
