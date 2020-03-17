55b221cd50a5e8c14b8a493607f6f28f416936cd
wilianx7 <wilianx7@hotmail.com>
2020-03-17 19:53:41 -0300

    Add HomeComponent

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {
+  NgtContentModule,
+  NgtHeaderNavModule,
+  NgtInputModule,
+  NgtSidenavModule,
+  NgtStylizableModule,
+  NgtSvgModule,
+} from 'projects/ng-tailwind/src/public-api';
+
+import { HomeComponent } from './home.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NgtHeaderNavModule,
+    NgtSvgModule,
+    NgtStylizableModule,
+    NgtSidenavModule,
+    NgtContentModule,
+    NgtInputModule
+  ]
+})
+export class HomeModule { }
