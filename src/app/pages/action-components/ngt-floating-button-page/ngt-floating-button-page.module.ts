5be68310d3aaeafe99c2641b99c616301bfda595
wilianx7 <wilianx7@hotmail.com>
2020-03-17 19:55:04 -0300

    Add NgtFloatingButtonPageComponent

---
+++ src/app/pages/action-components/ngt-floating-button-page/ngt-floating-button-page.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgtFloatingButtonModule, NgtPortletModule, NgtStylizableModule } from 'projects/ng-tailwind/src/public-api';
+import { CodePreviewModule } from 'src/app/components/code-preview/code-preview.module';
+
+import { NgtFloatingButtonPageComponent } from './ngt-floating-button-page.component';
+
+const routes: Routes = [
+  {
+    "path": '',
+    "component": NgtFloatingButtonPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [NgtFloatingButtonPageComponent],
+  imports: [
+    CommonModule,
+    CommonModule,
+    RouterModule.forChild(routes),
+    CodePreviewModule,
+    NgtPortletModule,
+    NgtFloatingButtonModule,
+    NgtStylizableModule
+  ]
+})
+export class NgtFloatingButtonPageModule { }
