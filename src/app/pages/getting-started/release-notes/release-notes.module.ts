7def0689586122f595079586a2e6219708c88386
wilianx7 <wilianx7@hotmail.com>
2020-03-17 19:54:09 -0300

    Add ReleaseNotesComponent

---
+++ src/app/pages/getting-started/release-notes/release-notes.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgtPortletModule, NgtSvgModule } from 'projects/ng-tailwind/src/public-api';
+
+import { ReleaseNotesComponent } from './release-notes.component';
+
+const routes: Routes = [
+  {
+    "path": '',
+    "component": ReleaseNotesComponent
+  }
+];
+
+@NgModule({
+  declarations: [ReleaseNotesComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    NgtPortletModule,
+    NgtSvgModule
+  ]
+})
+export class ReleaseNotesModule { }
