35926de05cbea639babe5fc50f0f50b11b606a92
wilianx7 <wilianx7@hotmail.com>
2020-03-19 14:54:24 -0300

    Change project name of lib-test for ng-tailwind-documentation

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('lib-test app is running!');
+    expect(page.getTitleText()).toEqual('ng-tailwind-documentation app is running!');
   });
 
   afterEach(async () => {
