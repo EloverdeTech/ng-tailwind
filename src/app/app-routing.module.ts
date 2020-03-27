8311d8c9d828d34f5c8b7b189fe73d5fbd0f58b5
wilianx7 <wilianx7@hotmail.com>
2020-03-27 08:51:53 -0300

    Update AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,8 @@ const routes: Routes = [
         "redirectTo": "installation",
         "pathMatch": "full"
       },
+
+      /** Getting Started */
       {
         "path": "installation",
         "loadChildren": () => import('src/app/pages/getting-started/installation/installation.module').then(m => m.InstallationModule)
@@ -27,6 +29,8 @@ const routes: Routes = [
         "path": "release-notes",
         "loadChildren": () => import('src/app/pages/getting-started/release-notes/release-notes.module').then(m => m.ReleaseNotesModule)
       },
+
+      /** Action Components */
       {
         "path": "ngt-action",
         "loadChildren": () => import('src/app/pages/action-components/ngt-action-page/ngt-action-page.module').then(m => m.NgtActionPageModule)
@@ -39,6 +43,8 @@ const routes: Routes = [
         "path": "ngt-floating-button",
         "loadChildren": () => import('src/app/pages/action-components/ngt-floating-button-page/ngt-floating-button-page.module').then(m => m.NgtFloatingButtonPageModule)
       },
+
+      /** Form Components */
       {
         "path": "ngt-checkbox",
         "loadChildren": () => import('src/app/pages/form-components/ngt-checkbox-page/ngt-checkbox-page.module').then(m => m.NgtCheckboxPageModule)
@@ -51,6 +57,8 @@ const routes: Routes = [
         "path": "ngt-dropzone",
         "loadChildren": () => import('src/app/pages/form-components/ngt-dropzone-page/ngt-dropzone-page.module').then(m => m.NgtDropzonePageModule)
       },
+
+      /** Table Components */
       {
         "path": "ngt-datatable",
         "loadChildren": () => import('src/app/pages/table-components/ngt-datatable-page/ngt-datatable-page.module').then(m => m.NgtDatatablePageModule)
