deade47eb94b887fc1fb115facc47a670a1cbb42
wilianx7 <wilianx7@hotmail.com>
2020-03-27 15:29:53 -0300

    Add NgtFormPageComponent

---
+++ src/app/pages/form-components/ngt-form-page/ngt-form-page.module.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  NgtButtonModule,
+  NgtFormModule,
+  NgtFormValidationMessageModule,
+  NgtInputModule,
+  NgtPortletModule,
+} from 'projects/ng-tailwind/src/public-api';
+import { CodePreviewModule } from 'src/app/components/code-preview/code-preview.module';
+
+import { NgtFormPageComponent } from './ngt-form-page.component';
+
+const routes: Routes = [
+  {
+    "path": '',
+    "component": NgtFormPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [NgtFormPageComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    CodePreviewModule,
+    NgtPortletModule,
+    NgtFormModule,
+    NgtInputModule,
+    NgtButtonModule,
+    NgtFormValidationMessageModule
+  ]
+})
+export class NgtFormPageModule { }
