46e486aa8b466bd7d6bea8b39b3eb5f4be0fef1d
wilianx7 <wilianx7@hotmail.com>
2020-03-30 14:14:32 -0300

    Add NgtInputPageComponent

---
+++ src/app/pages/form-components/ngt-input-page/ngt-input-page.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngt-input-page',
+  templateUrl: './ngt-input-page.component.html',
+  styleUrls: ['./ngt-input-page.component.css']
+})
+export class NgtInputPageComponent implements OnInit {
+  public allowClearInput: string;
+  public decimalInput: string;
+  public cpfCnpj: string;
+  public password: string;
+  public passwordConfirm: string;
+
+  public codeExample = `
+  <ngt-input class="block" [jit]='true' name='allowClearInput' 
+    [(ngModel)]="allowClearInput" label='Allow Clear Input' 
+    [isRequired]='true' [allowClear]='true'>
+  </ngt-input>
+
+  <ngt-input class="block" name='decimalInput' 
+    [(ngModel)]="decimalInput" label='Decimal Input'
+    innerLeftIcon='assets/images/icons/keyboard.svg' mask='decimal'>
+  </ngt-input>
+
+  <ngt-input class="block" name='cpfCnpj' label='CPF/CNPJ' [(ngModel)]="cpfCnpj" 
+    mask='cnpj-cpf'>
+  </ngt-input>
+
+  <ngt-input class="block" name='passwordInput' label='Password' type='password'
+    [(ngModel)]="password">
+  </ngt-input>
+
+  <ngt-input class="block" name='passwordConfirm' label='Password Confirm' 
+    type='password' [(ngModel)]="passwordConfirm" [match]='password'>
+  </ngt-input>
+  `;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
