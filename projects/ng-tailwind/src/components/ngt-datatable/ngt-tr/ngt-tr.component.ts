dd2367c39bcd2be9e198f6944f6336f7f55648c4
wilianx7 <wilianx7@hotmail.com>
2020-04-30 13:49:12 -0300

    Add stripped table -- NgtTrComponent

--- projects/ng-tailwind/src/components/ngt-datatable/ngt-tr/ngt-tr.component.ts
+++ projects/ng-tailwind/src/components/ngt-datatable/ngt-tr/ngt-tr.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '[ngt-tr]',
   templateUrl: './ngt-tr.component.html',
   styleUrls: ['./ngt-tr.component.css'],
-  host: { class: 'border-r border-t md:border-r-0 md:border-t-0' }
+  host: { class: 'border-r border-t md:border-r-0 md:border-t-0' },
+  encapsulation: ViewEncapsulation.None
 })
-export class NgtTrComponent { }
+export class NgtTrComponent {
+  @HostBinding('class.evenStripped') @Input() evenStripped: boolean;
+  @HostBinding('class.oddStripped') @Input() oddStripped: boolean;
+}
