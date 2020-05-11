b63cfd981baea0033a7f1669d8d810600b02a5d3
wilianx7 <wilianx7@hotmail.com>
2020-05-11 09:33:16 -0300

    Init NgtContentPage -- Documentation

---
+++ src/app/pages/layout-components/ngt-content-page/ngt-content-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngt-content-page',
+  templateUrl: './ngt-content-page.component.html',
+  styleUrls: ['./ngt-content-page.component.css']
+})
+export class NgtContentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/layout-components/ngt-content-page/ngt-content-page.module.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  NgtContentModule,
+  NgtInputModule,
+  NgtPortletModule,
+  NgtStylizableModule,
+} from 'projects/ng-tailwind/src/public-api';
+import { CodePreviewModule } from 'src/app/components/code-preview/code-preview.module';
+import { NgtStylizableTemplateModule } from 'src/app/components/ngt-stylizable-template/ngt-stylizable-template.module';
+
+import { NgtContentPageComponent } from './ngt-content-page.component';
+
+const routes: Routes = [
+  {
+    "path": '',
+    "component": NgtContentPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [NgtContentPageComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    CodePreviewModule,
+    NgtPortletModule,
+    NgtContentModule,
+    NgtStylizableTemplateModule,
+    NgtStylizableModule,
+    NgtInputModule
+  ]
+})
+export class NgtContentPageModule { }
