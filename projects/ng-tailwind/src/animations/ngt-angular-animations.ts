4cbd38b0ceb01bdffc4fc9574d78114319b3bcb3
wilianx7 <wilianx7@hotmail.com>
2020-06-02 11:12:44 -0300

    Add slideRightToLeftAnimation -- NgtAngularAnimations

--- projects/ng-tailwind/src/animations/ngt-angular-animations.ts
+++ projects/ng-tailwind/src/animations/ngt-angular-animations.ts
@@ -9,6 +9,15 @@ export function slideLeftToRightAnimation(name: string, duration: any, customSty
     ]);
 }
 
+export function slideRightToLeftAnimation(name: string, duration: any, customStyle?: AnimationStyleMetadata) {
+    return trigger(name, [
+        state('void', customStyle ? customStyle : style({ transform: 'translateX(50px)', opacity: 0 })),
+        transition(':enter, :leave', [
+            animate(duration)
+        ])
+    ]);
+}
+
 export function fadeDownAnimation(name: string, duration: any, customStyle?: AnimationStyleMetadata) {
     return trigger(name, [
         state('void', customStyle ? customStyle : style({ transform: 'translateY(-20px)', opacity: 0 })),
