63a3adc43f55b65b5208379a152548ee82df55ea
nicollasmatheus37 <nicollasmatheus37@gmail.com>
2021-01-29 09:48:53 -0300

    #46 Added preview mode -- NgtDropzone

--- projects/ng-tailwind/src/components/ngt-dropzone/ngt-dropzone.module.ts
+++ projects/ng-tailwind/src/components/ngt-dropzone/ngt-dropzone.module.ts
@@ -1,21 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { NgxViewerModule } from 'ngx-viewer';
 
 import { NgtShiningModule } from '../ngt-shining/ngt-shining.module';
+import { NgtSvgModule } from '../ngt-svg/ngt-svg.module';
 import { NgtValidationModule } from '../ngt-validation/ngt-validation.module';
+import { NgtDropzoneFileViewerComponent } from './ngt-dropzone-file-viewer/ngt-dropzone-file-viewer.component';
 import { NgtDropzoneComponent } from './ngt-dropzone.component';
 
 @NgModule({
-    declarations: [NgtDropzoneComponent],
+    declarations: [NgtDropzoneComponent, NgtDropzoneFileViewerComponent],
     exports: [NgtDropzoneComponent],
     imports: [
         CommonModule,
         FormsModule,
         NgtValidationModule,
         NgtShiningModule,
-        NgxDropzoneModule
+        NgxDropzoneModule,
+        NgxViewerModule,
+        NgtSvgModule,
+        NgxDocViewerModule
     ]
 })
 export class NgtDropzoneModule { }
