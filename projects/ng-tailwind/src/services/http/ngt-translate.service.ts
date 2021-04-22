c39b9292dc9d75d010b0278f8b8302c0ff96eb38
nicollasmatheus37 <nicollasmatheus37@gmail.com>
2021-04-22 20:50:51 -0300

    Fix translate service properties

--- projects/ng-tailwind/src/services/http/ngt-translate.service.ts
+++ projects/ng-tailwind/src/services/http/ngt-translate.service.ts
@@ -1,34 +1,34 @@
 export abstract class NgtTranslateService {
-    protected abstract ngtFormDefaultValidationMessage: string;
+    public abstract ngtFormDefaultValidationMessage: string;
 
-    protected abstract ngtDatatableNoDataFound: string;
+    public abstract ngtDatatableNoDataFound: string;
 
-    protected abstract ngtSelectLoadingText: string;
-    protected abstract ngtSelectTypeToSearchText: string;
-    protected abstract ngtSelectClearAllTooltip: string;
-    protected abstract ngtSelectPlaceholder: string;
-    protected abstract ngtSelectCreateText: string;
+    public abstract ngtSelectLoadingText: string;
+    public abstract ngtSelectTypeToSearchText: string;
+    public abstract ngtSelectClearAllTooltip: string;
+    public abstract ngtSelectPlaceholder: string;
+    public abstract ngtSelectCreateText: string;
 
-    protected abstract ngtTextAreaRemainingCharacters: string;
+    public abstract ngtTextAreaRemainingCharacters: string;
 
-    protected abstract ngtValidationMaxCharactersExceded: string;
-    protected abstract ngtValidationRequiredField: string;
-    protected abstract ngtValidationInvalidEmail: string;
-    protected abstract ngtValidationAlreadyExists: string;
-    protected abstract ngtValidationInvalidCnpj: string;
-    protected abstract ngtValidationInvalidCpf: string;
-    protected abstract ngtValidationPasswordRequiredMinCharacters: string;
-    protected abstract ngtValidationFieldsNotMatch: string;
-    protected abstract ngtValidationInvalidDate: string;
-    protected abstract ngtValidationInvalidPrevision: string;
-    protected abstract ngtValidationExternalServerUnavailable: string;
+    public abstract ngtValidationMaxCharactersExceded: string;
+    public abstract ngtValidationRequiredField: string;
+    public abstract ngtValidationInvalidEmail: string;
+    public abstract ngtValidationAlreadyExists: string;
+    public abstract ngtValidationInvalidCnpj: string;
+    public abstract ngtValidationInvalidCpf: string;
+    public abstract ngtValidationPasswordRequiredMinCharacters: string;
+    public abstract ngtValidationFieldsNotMatch: string;
+    public abstract ngtValidationInvalidDate: string;
+    public abstract ngtValidationInvalidPrevision: string;
+    public abstract ngtValidationExternalServerUnavailable: string;
 
     public get(varName: string) {
         return this[varName];
     }
 
-    protected abstract ngtValidationValueMustBeGreaterThan(minValue?: number | string): string;
-    protected abstract ngtValidationMinLengthField(minLenth?: number | string): string;
+    public abstract ngtValidationValueMustBeGreaterThan(minValue?: number | string): string;
+    public abstract ngtValidationMinLengthField(minLenth?: number | string): string;
 
-    protected abstract ngtPagination(from: number | string, to: number | string, total: number | string): string;
+    public abstract ngtPagination(from: number | string, to: number | string, total: number | string): string;
 }
