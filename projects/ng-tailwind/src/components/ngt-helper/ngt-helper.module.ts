e071525f13aa709be16c3bd61ab31efc58160b1a
nicollasmatheus37 <nicollasmatheus37@gmail.com>
2021-05-04 11:14:28 -0300

    Removed sat-popover

--- projects/ng-tailwind/src/components/ngt-helper/ngt-helper.module.ts
+++ projects/ng-tailwind/src/components/ngt-helper/ngt-helper.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { SatPopoverModule } from "@ncstate/sat-popover";
 import { NgtSvgModule } from "../ngt-svg/ngt-svg.module";
 import { NgtHelperComponent } from "./ngt-helper.component";
 import { NgtDropdownModule } from '../ngt-dropdown/ngt-dropdown.module';
@@ -11,7 +10,6 @@ import { NgtDropdownModule } from '../ngt-dropdown/ngt-dropdown.module';
     imports: [
         CommonModule,
         NgtSvgModule,
-        SatPopoverModule,
         NgtDropdownModule
     ]
 })
