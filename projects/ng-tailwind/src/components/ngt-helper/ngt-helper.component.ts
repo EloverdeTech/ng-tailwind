ba5a08024ec9386d31171f36d536a1659e87c4ba
wilianx7 <wilianx7@hotmail.com>
2021-07-05 08:30:26 -0300

    Remove X position reverse implementation -- NgtHelper

--- projects/ng-tailwind/src/components/ngt-helper/ngt-helper.component.ts
+++ projects/ng-tailwind/src/components/ngt-helper/ngt-helper.component.ts
@@ -9,32 +9,13 @@ import { NgtTranslateService } from '../../services/http/ngt-translate.service';
 })
 export class NgtHelperComponent {
     @ViewChild('dropdownRef', { static: true }) public dropdownRef: ElementRef;
-    @ViewChild('dropdownContainerRef', { static: true }) public dropdownContainerRef: ElementRef;
 
-    @Input() public icon: string = '';
-    @Input() public helpTitle: string = '';
-    @Input() public iconColor: string = '';
-    @Input() public showHelperOnTop: boolean = false;
-
-    private dropdownContainerXPosition: number;
+    @Input() public icon: string;
+    @Input() public helpTitle: string;
+    @Input() public iconColor: string;
 
     public constructor(
         @Optional()
         public ngtTranslateService: NgtTranslateService
     ) { }
-
-    public reversePositionX(): boolean {
-        this.bindDropdownContainerXPosition();
-
-        return !(this.dropdownContainerXPosition > document.documentElement.clientWidth);
-    }
-
-    private bindDropdownContainerXPosition() {
-        if (!this.dropdownContainerXPosition && this.dropdownContainerRef.nativeElement.offsetWidth) {
-            setTimeout(() => {
-                this.dropdownContainerXPosition = this.dropdownContainerRef.nativeElement.getBoundingClientRect().x
-                    + this.dropdownContainerRef.nativeElement.offsetWidth;
-            });
-        }
-    }
 }
