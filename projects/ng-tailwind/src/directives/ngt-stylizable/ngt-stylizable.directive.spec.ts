aa036c6bf41aa2501a227be60072c9da011dd0b6
wilianx7 <wilianx7@hotmail.com>
2023-03-21 10:40:49 -0300

    refactor: delete e2e tests

--- projects/ng-tailwind/src/directives/ngt-stylizable/ngt-stylizable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NgtStylizableDirective } from './ngt-stylizable.directive';
-
-describe('NgtStylizableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgtStylizableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
