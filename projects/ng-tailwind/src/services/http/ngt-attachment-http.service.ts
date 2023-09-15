78de9de078e28bdb3ae57654124a32cf24e216db
wilianx7 <wilianx7@hotmail.com>
2023-09-15 17:58:28 -0300

    refactor (NgtAttachmentHttpService): remove useless "thumbnail" method

--- projects/ng-tailwind/src/services/http/ngt-attachment-http.service.ts
+++ projects/ng-tailwind/src/services/http/ngt-attachment-http.service.ts
@@ -3,8 +3,6 @@ import { Observable } from 'rxjs';
 export abstract class NgtAttachmentHttpService {
     public abstract preview(attachment: any): Observable<NgtAttachmentHttpResponse>;
 
-    public abstract thumbnail(attachment: any): Observable<NgtAttachmentHttpResponse>;
-
     public abstract download(attachment: any): Observable<any>;
 
     public abstract upload(remoteResource: any, file: any): Observable<any>;
