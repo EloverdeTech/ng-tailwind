a0869786b5aa373a65b14fb7eafdb66ea82ccf61
deividss23 <deivid1959@live.com>
2024-08-28 15:00:51 -0300

    feat(ngtModalFooter): create constructor to apply styles

--- projects/ng-tailwind/src/components/ngt-modal/ngt-modal-footer/ngt-modal-footer.component.ts
+++ projects/ng-tailwind/src/components/ngt-modal/ngt-modal-footer/ngt-modal-footer.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Injector, Input, Optional, Self } from '@angular/core';
+import { NgtStylizableDirective } from '../../../directives/ngt-stylizable/ngt-stylizable.directive';
+import { NgtStylizableService } from '../../../services/ngt-stylizable/ngt-stylizable.service';
 
 @Component({
     selector: 'ngt-modal-footer',
     templateUrl: './ngt-modal-footer.component.html'
 })
-export class NgtModalFooterComponent { }
+export class NgtModalFooterComponent {
+    @Input() public ngtStyle: NgtStylizableService;
+
+    public constructor(
+        private injector: Injector,
+
+        @Self() @Optional()
+        private tailStylizableDirective: NgtStylizableDirective,
+    ) {
+        if (this.tailStylizableDirective) {
+            this.ngtStyle = this.tailStylizableDirective.getNgtStylizableService();
+        } else if (!this.ngtStyle) {
+            this.ngtStyle = new NgtStylizableService();
+        }
+
+        this.ngtStyle.load(this.injector, 'NgtModalFooter', {
+            px: 'px-0',
+            py: 'py-1'
+        });
+    }
+}
