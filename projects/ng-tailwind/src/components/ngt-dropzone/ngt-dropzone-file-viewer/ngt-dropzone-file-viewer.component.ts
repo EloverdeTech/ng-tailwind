e0d2ad97d08ff904f2a35480c42106e93b651c50
vitorAllux <vitor@eloverde.com.br>
2024-10-11 16:49:48 -0300

    fix (NgtDropzoneFileViewer): Add timeout with 10 seconds to hide loading

--- projects/ng-tailwind/src/components/ngt-dropzone/ngt-dropzone-file-viewer/ngt-dropzone-file-viewer.component.ts
+++ projects/ng-tailwind/src/components/ngt-dropzone/ngt-dropzone-file-viewer/ngt-dropzone-file-viewer.component.ts
@@ -41,6 +41,10 @@ export class NgtDropzoneFileViewerComponent {
             this.loading = true;
             this.canShowViewer = true;
         }
+
+        setTimeout(() => {
+            this.loading = false;
+        }, 10000);
     }
 
     public close(): void {
