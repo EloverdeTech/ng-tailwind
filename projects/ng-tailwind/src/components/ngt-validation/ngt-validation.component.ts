8c0e06becf2211cfdbc2d6c7d76b0cf3143f7f64
wilianx7 <wilianx7@hotmail.com>
2025-11-13 07:27:04 -0300

    feat (NgtValidation): handle form group and use control flow

--- projects/ng-tailwind/src/components/ngt-validation/ngt-validation.component.ts
+++ projects/ng-tailwind/src/components/ngt-validation/ngt-validation.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, Optional } from '@angular/core';
-import { ControlContainer, UntypedFormControl } from '@angular/forms';
+import { Component, Input, Optional, SkipSelf } from '@angular/core';
+import { ControlContainer, FormGroupDirective, UntypedFormControl } from '@angular/forms';
 
 import { NgtTranslateService } from '../../services/http/ngt-translate.service';
 
@@ -9,13 +9,40 @@ import { NgtTranslateService } from '../../services/http/ngt-translate.service';
     standalone: false
 })
 export class NgtValidationComponent {
-    @Input() public control: UntypedFormControl;
+    @Input({ required: true }) public control: UntypedFormControl;
     @Input() public container: ControlContainer;
     @Input() public minValue: number;
     @Input() public minLength: number;
 
     public constructor(
         @Optional()
-        public ngtTranslateService: NgtTranslateService
+        public ngtTranslateService: NgtTranslateService,
+
+        @Optional() @SkipSelf()
+        private formGroup: FormGroupDirective,
     ) { }
+
+    public get isSubmitted(): boolean {
+        if (this.formGroup) {
+            return this.formGroup.submitted;
+        }
+
+        return this.container?.['submitted'];
+    }
+
+    public get isDirty(): boolean {
+        return this.control?.dirty;
+    }
+
+    public get isTouched(): boolean {
+        return this.control?.touched;
+    }
+
+    public shouldShowError(error: string): boolean {
+        return this.hasError(error) && (this.isTouched || this.isSubmitted);
+    }
+
+    private hasError(error: string): boolean {
+        return this.control?.errors?.[error];
+    }
 }
