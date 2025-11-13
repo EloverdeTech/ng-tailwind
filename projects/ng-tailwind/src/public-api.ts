312d971ecceac2382252ab09196b5f66ecde92f2
wilianx7 <wilianx7@hotmail.com>
2025-11-13 07:29:20 -0300

    refactor: reorganize public api exports

--- projects/ng-tailwind/src/public-api.ts
+++ projects/ng-tailwind/src/public-api.ts
@@ -2,22 +2,35 @@
  * Public API Surface of ng-tailwind
  */
 
-//Translate
+/** Services */
+
+// NgtTranslateService
 export * from './services/http/ngt-translate.service';
 
-//NgtHttpService
+// NgtHttpService
 export * from './services/http/ngt-http.service';
 
-//NgtHttpValidationService
+// NgtHttpValidationService
 export * from './services/http/ngt-http-validation.service';
 
-//NgtHttpResourceService
+// NgtHttpResourceService
 export * from './services/http/ngt-http-resource.service';
 
-//NgtAbilityValidationService
+// NgtAbilityValidationService
 export * from './services/validation/ngt-ability-validation.service';
 
-//NgtDatatable
+// NgtHttpFormService
+export * from './services/http/ngt-http-form.service';
+
+/** Directives */
+
+// NgtStylizable
+export * from './directives/ngt-stylizable/ngt-stylizable.module';
+export * from './directives/ngt-stylizable/ngt-stylizable.directive';
+
+/** List Components */
+
+// NgtDatatable
 export * from './components/ngt-datatable/ngt-datatable.module';
 export * from './components/ngt-datatable/ngt-datatable.component';
 export * from './components/ngt-datatable/ngt-tbody/ngt-tbody.component';
@@ -28,60 +41,33 @@ export * from './components/ngt-datatable/ngt-th-check/ngt-th-check.component';
 export * from './components/ngt-datatable/ngt-thead/ngt-thead.component';
 export * from './components/ngt-datatable/ngt-tr/ngt-tr.component';
 
-//NgtPagination
+// NgtPagination
 export * from './components/ngt-pagination/ngt-pagination.module';
 export * from './components/ngt-pagination/ngt-pagination.component';
 
-//NgtDropdown
+/** General Components */
+
+// NgtDropdown
 export * from './components/ngt-dropdown/ngt-dropdown.module';
 export * from './components/ngt-dropdown/ngt-dropdown.component';
 export * from './components/ngt-dropdown/ngt-dropdown-container/ngt-dropdown-container.component';
 
-//NgtFloatingButton
-export * from './components/ngt-floating-button/ngt-floating-button.module';
-export * from './components/ngt-floating-button/ngt-floating-button.component';
-
-//NgtTag
+// NgtTag
 export * from './components/ngt-tag/ngt-tag.module';
 export * from './components/ngt-tag/ngt-tag.component';
 
-//NgtAction
+// NgtAction
 export * from './components/ngt-action/ngt-action.module';
 export * from './components/ngt-action/ngt-action.component';
 
-//NgtCheckbox
-export * from './components/ngt-checkbox/ngt-checkbox.module';
-export * from './components/ngt-checkbox/ngt-checkbox.component';
-
-//NgtSlider
-export * from './components/ngt-slider/ngt-slider.module';
-export * from './components/ngt-slider/ngt-slider.component';
-
-//NgtRadioButton
-export * from './components/ngt-radio-button/ngt-radio-button.module';
-export * from './components/ngt-radio-button/ngt-radio-button.component';
-export * from './components/ngt-radio-button/ngt-radio-button-container/ngt-radio-button-container.component';
-
-//NgtTextarea
-export * from './components/ngt-textarea/ngt-textarea.module';
-export * from './components/ngt-textarea/ngt-textarea.component';
-
-//NgtModal
+// NgtModal
 export * from './components/ngt-modal/ngt-modal.module';
 export * from './components/ngt-modal/ngt-modal.component';
 export * from './components/ngt-modal/ngt-modal-header/ngt-modal-header.component';
 export * from './components/ngt-modal/ngt-modal-body/ngt-modal-body.component';
 export * from './components/ngt-modal/ngt-modal-footer/ngt-modal-footer.component';
 
-//NgtDate
-export * from './components/ngt-date/ngt-date.module';
-export * from './components/ngt-date/ngt-date.component';
-
-//NgtButton
-export * from './components/ngt-button/ngt-button.module';
-export * from './components/ngt-button/ngt-button.component';
-
-//NgtPortlet
+// NgtPortlet
 export * from './components/ngt-portlet/ngt-portlet.module';
 export * from './components/ngt-portlet/ngt-portlet.component';
 export * from './components/ngt-portlet/ngt-portlet-header/ngt-portlet-header.component';
@@ -89,63 +75,9 @@ export * from './components/ngt-portlet/ngt-portlet-body/ngt-portlet-body.compon
 export * from './components/ngt-portlet/ngt-portlet-footer/ngt-portlet-footer.component';
 export * from './enums/size.enum';
 
-//NgtSidenav
-export * from './components/ngt-sidenav/ngt-sidenav.module';
-export * from './components/ngt-sidenav/ngt-sidenav.component';
-
-//NgtHeader
-export * from './components/ngt-header-nav/ngt-header-nav.module';
-export * from './components/ngt-header-nav/ngt-header-nav.component';
-
 //NgtContent
 export * from './components/ngt-content/ngt-content.module';
 export * from './components/ngt-content/ngt-content.component';
-
-// NgtSection
-export * from './components/ngt-section/ngt-section.module';
-export * from './components/ngt-section/ngt-section.component';
-
-//NgtInput
-export * from './components/ngt-input/ngt-input.module';
-export * from './components/ngt-input/ngt-input.component';
-
-//NgtStylizable
-export * from './directives/ngt-stylizable/ngt-stylizable.module';
-export * from './directives/ngt-stylizable/ngt-stylizable.directive';
-
-//NgtSelect
-export * from './components/ngt-select/ngt-select.module';
-export * from './components/ngt-select/ngt-select.component';
-export * from './components/ngt-select/ngt-select.directive';
-
-//NgtSelectContainer
-export * from './components/ngt-multi-select/ngt-multi-select.module';
-export * from './components/ngt-multi-select/ngt-multi-select.component';
-
-//NgtSvg
-export * from './components/ngt-svg/ngt-svg.module';
-export * from './components/ngt-svg/ngt-svg.component';
-
-//NgtShining
-export * from './components/ngt-shining/ngt-shining.module';
-export * from './components/ngt-shining/ngt-shining.component';
-
-//NgtForm
-export * from './components/ngt-form/ngt-form.module';
-export * from './components/ngt-form/ngt-form.component';
-export * from './components/ngt-form/ngt-form-validation-message/ngt-form-validation-message.component';
-
-//NgtHttpFormService
-export * from './services/http/ngt-http-form.service';
-
-//NgtDropzone
-export * from './components/ngt-dropzone/ngt-dropzone.module';
-export * from './components/ngt-dropzone/ngt-dropzone.component';
-export * from './components/ngt-dropzone/ngt-dropzone.meta';
-export * from './services/http/ngt-attachment-http.service';
-
-//NgtAnimations
-export * from './animations/ngt-angular-animations';
 
 // NgtHelper
 export * from './components/ngt-helper/ngt-helper.module';
@@ -160,3 +92,92 @@ export * from './components/ngt-popover/ngt-popover.directive';
 export * from './components/ngt-context-menu/ngt-context-menu.module';
 export * from './components/ngt-context-menu/ngt-context-menu.component';
 export * from './components/ngt-context-menu/ngt-context-menu.directive';
+
+// NgtSvg
+export * from './components/ngt-svg/ngt-svg.module';
+export * from './components/ngt-svg/ngt-svg.component';
+
+// NgtShining
+export * from './components/ngt-shining/ngt-shining.module';
+export * from './components/ngt-shining/ngt-shining.component';
+
+/** Navigation/Menu Components */
+
+// NgtSidenav
+export * from './components/ngt-sidenav/ngt-sidenav.module';
+export * from './components/ngt-sidenav/ngt-sidenav.component';
+
+// NgtHeaderNav
+export * from './components/ngt-header-nav/ngt-header-nav.module';
+export * from './components/ngt-header-nav/ngt-header-nav.component';
+
+/** Form Components */
+
+// NgtCheckbox
+export * from './components/ngt-checkbox/ngt-checkbox.module';
+export * from './components/ngt-checkbox/ngt-checkbox.component';
+
+// NgtFloatingButton
+export * from './components/ngt-floating-button/ngt-floating-button.module';
+export * from './components/ngt-floating-button/ngt-floating-button.component';
+
+// NgtSlider
+export * from './components/ngt-slider/ngt-slider.module';
+export * from './components/ngt-slider/ngt-slider.component';
+
+// NgtRadioButton
+export * from './components/ngt-radio-button/ngt-radio-button.module';
+export * from './components/ngt-radio-button/ngt-radio-button.component';
+export * from './components/ngt-radio-button/ngt-radio-button-container/ngt-radio-button-container.component';
+
+// NgtSection
+export * from './components/ngt-section/ngt-section.module';
+export * from './components/ngt-section/ngt-section.component';
+
+/** Template-driven Form Components */
+
+// NgtTextarea
+export * from './components/ngt-textarea/ngt-textarea.module';
+export * from './components/ngt-textarea/ngt-textarea.component';
+
+// NgtDate
+export * from './components/ngt-date/ngt-date.module';
+export * from './components/ngt-date/ngt-date.component';
+
+// NgtButton
+export * from './components/ngt-button/ngt-button.module';
+export * from './components/ngt-button/ngt-button.component';
+
+// NgtInput
+export * from './components/ngt-input/ngt-input.module';
+export * from './components/ngt-input/ngt-input.component';
+
+// NgtSelect
+export * from './components/ngt-select/ngt-select.module';
+export * from './components/ngt-select/ngt-select.component';
+export * from './components/ngt-select/ngt-select.directive';
+
+// NgtMultiSelect
+export * from './components/ngt-multi-select/ngt-multi-select.module';
+export * from './components/ngt-multi-select/ngt-multi-select.component';
+
+// NgtForm
+export * from './components/ngt-form/ngt-form.module';
+export * from './components/ngt-form/ngt-form.component';
+export * from './components/ngt-form/ngt-form-validation-message/ngt-form-validation-message.component';
+
+// NgtDropzone
+export * from './components/ngt-dropzone/ngt-dropzone.module';
+export * from './components/ngt-dropzone/ngt-dropzone.component';
+export * from './components/ngt-dropzone/ngt-dropzone.meta';
+export * from './services/http/ngt-attachment-http.service';
+
+/** Reactive Form Components */
+
+// NgtReactInput
+export * from './components/forms/reactive/ngt-react-input/ngt-react-input.component';
+
+/** Helpers */
+
+//NgtAnimations
+export * from './animations/ngt-angular-animations';
